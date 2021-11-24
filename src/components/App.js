835daa99b8d7b8180717ca3f7bf8b7b07921519f
Mohd Numan <mohammadnuman123@gmail.com>
2021-11-24 15:41:37 +0530

    added react router:1 corrected

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { BrowserRouter as Router,Link,Route } from 'react-router-dom';
 import {fetchPosts} from '../actions/posts';
 import {PostsList} from './';
 import {Navbar} from './index';
+
+
+
+const Home=()=>{
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
 
 
 class App extends React.Component {
@@ -14,12 +25,14 @@ class App extends React.Component {
     const {posts}=this.props;
     // console.log("props",this.props);
     return (
+      <Router>
       <div>
         <Navbar/>
-        <PostsList posts={posts}/>
-        
-
+        {/* <PostsList posts={posts}/> */}
+        <Route exact path="/" component={Home}/>
+         
       </div>
+      </Router>
     );
   }
 }
