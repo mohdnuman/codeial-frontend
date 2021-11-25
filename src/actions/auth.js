3c133cbe039c3b3912d9b7556148aa242d2c1857
Mohd Numan <mohammadnuman123@gmail.com>
2021-11-25 18:30:12 +0530

    persisted the user 1

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -37,6 +37,7 @@ export function login(email,password){
         .then(data=>{
             console.log(data); 
             if(data.success){
+                localStorage.setItem('token',data.data.token);
                 dispatch(loginSuccess(data.data.user))
                 return;
             }
